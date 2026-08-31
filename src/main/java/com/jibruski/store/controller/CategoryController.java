6f6cb4c12b92c1daa1f198b73ed349f95fdec183
n-abrij <omollojibran@gmail.com>
2026-08-31 18:27:42 +0300

    feat: category CRUD ops

---
+++ src/main/java/com/jibruski/store/controller/CategoryController.java
@@ -0,0 +1,48 @@
+package com.jibruski.store.controller;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jibruski.store.dto.CategoryDto.CategoryRequest;
+import com.jibruski.store.dto.CategoryDto.CategoryResponse;
+import com.jibruski.store.service.CategoryService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponse>> getAll(){
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> create(@Valid @RequestBody CategoryRequest request){
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(request));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<CategoryResponse> update (@PathVariable Long id, @Valid @RequestBody CategoryRequest request){
+        return ResponseEntity.ok(categoryService.update(id, request));
+    }
+
+    @PatchMapping("/{id}/remove")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/jibruski/store/service/CategoryService.java
@@ -0,0 +1,81 @@
+package com.jibruski.store.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.jibruski.store.domain.Category;
+import com.jibruski.store.domain.User;
+import com.jibruski.store.dto.CategoryDto.CategoryRequest;
+import com.jibruski.store.dto.CategoryDto.CategoryResponse;
+import com.jibruski.store.repository.CategoryRepository;
+import com.jibruski.store.repository.UserRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+    private final CategoryRepository categoryRepository;
+    private final UserRepository userRepository;
+    private final CurrentUserService userService;
+
+    public List<CategoryResponse> getAll(){
+        return categoryRepository.findByUserIdAndActiveTrue(userService.getCurrentUserId())
+            .stream()
+            .map(CategoryResponse::fromEntity)
+            .toList();
+    }
+
+    @Transactional
+    public CategoryResponse create(CategoryRequest request){
+        Category existing = categoryRepository.findByName(request.name()).orElse(null);
+
+        if(existing != null && existing.getUser().getId().equals(userService.getCurrentUserId())){
+            throw new RuntimeException("Category already exists");
+        }
+
+        Category category = new Category();
+        category.setName(request.name());
+        category.setSlug(request.slug());
+        category.setUser(getCurrentUser());
+        category.setActive(true);
+
+        categoryRepository.save(category);
+        return CategoryResponse.fromEntity(category);
+    }
+
+    @Transactional
+    public CategoryResponse update(Long id, CategoryRequest request){
+        Category category = getCategory(id);
+
+        if(request.name() != null) category.setName(request.name());
+        if(request.slug() != null) category.setSlug(request.slug());
+
+        categoryRepository.save(category);
+        return CategoryResponse.fromEntity(category);
+    }
+
+    public void delete(Long id){
+        Category category = getCategory(id);
+        category.setActive(false);
+
+        categoryRepository.save(category);
+    }
+
+    private User getCurrentUser() {
+        Long userId = userService.getCurrentUserId();
+        return userRepository.findById(userId)
+            .orElseThrow(() -> new IllegalStateException("Authenticated user not found in database"));
+    }
+
+    private Category getCategory(Long id){
+        Category category = categoryRepository.findByIdAndActiveTrue(id).orElse(null);
+        if(category == null || !category.getUser().getId().equals(userService.getCurrentUserId())){
+            throw new RuntimeException("Category not found");
+        }
+
+        return category;
+    }
+}
